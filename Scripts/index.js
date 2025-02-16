729e395450ae2457de5f8203861d0d907554185c
Gustavo <gugacontas1303@gmail.com>
2025-02-16 01:41:02 -0300

    Feat: implementing pagination

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,29 +1,57 @@
 new Vue({
     el: "#app",
     data: {
-        heroes: [] 
+        heroes: [] ,
+        currentPage: 1,
+        maxPage: 0,
     },
     async mounted() {
-        await this.fetchHeroes();
+        let currentPage = await this.getParameter('page') || 1;
+        this.currentPage = currentPage;
+        await this.fetchHeroes(currentPage);
     },
     methods: {
-        async fetchHeroes() {
+        async fetchHeroes(currentPage) {
+            
+            const limit = 20;
+            const offset = (currentPage - 1) * limit;
+            
             const ts = "1"; 
             const publicKey = "15cea5f8a59b71e7d636187219977051";
             const hash = "62eb37bec60ace73acd483210bc3fc51"; 
 
-            const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+            const url = `http://gateway.marvel.com/v1/public/characters?&offset=${offset}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
 
             try {
                     const response = await fetch(url);
                     const data = await response.json();
 
-                    data.code === 200 ? this.heroes = data.data.results : console.error("API ERROR:", data.status);
+                    if(data.code === 200) {
+                        this.heroes = data.data.results
+
+                        const totalHeroes = data.data.total;
+                        this.maxPage = Math.ceil(totalHeroes/limit);
+                        
+                    } else{
+                        console.error("API ERROR:", data.status);
+                    }
 
             } catch (error) {
                 console.error("REQUEST ERROR:", error);
             }
+
+            console.log(this.maxPage)
         },
+
+        async getParameter(param){
+            const urlParams = new URLSearchParams(window.location.search);
+            return urlParams.get(param);
+        },
+
+        async changePage(){
+            window.location.href = `index.html?page=${newPage}`;
+        },
+
         async heroPage(id) {
             window.location.href = `heroPage.html?heroId=${id}`;
         }
